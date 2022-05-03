297b8219507e10cc4dc04c5811b86d3622911fd7
Makoto Hiramatsu <himkt@klis.tsukuba.ac.jp>
2022-05-03 11:45:09 +0900

    Implement task

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,14 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
 
-export function activate(context: vscode.ExtensionContext) {
+
+const type = 'cargoAtcoderProvider';
+
+
+export async function activate(context: vscode.ExtensionContext) {
+
+	// (1). command
+
 	let cargoRun = (type: String) => {
 		let currentFilePath = vscode.window.activeTextEditor?.document.fileName!;
 		let basename = path.parse(currentFilePath).name;
@@ -23,6 +30,42 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(testCommand);
 	context.subscriptions.push(submitCommand);
+
+	// (2). tasks
+
+    vscode.tasks.registerTaskProvider(type, {
+        provideTasks(token?: vscode.CancellationToken) {
+			const currentFilePath = vscode.window.activeTextEditor?.document.fileName!;
+			const basename = path.parse(currentFilePath).name;
+
+            return [
+                new vscode.Task(
+					{ type: type, command: "status" },
+					vscode.TaskScope.Workspace,
+                    "status",
+					"cargo-atcoder",
+					new vscode.ShellExecution(`cargo atcoder status`)
+				),
+                new vscode.Task(
+					{ type: type, command: "test" },
+					vscode.TaskScope.Workspace,
+                    "test",
+					"cargo-atcoder",
+					new vscode.ShellExecution(`cargo atcoder test ${basename}`)
+				),
+                new vscode.Task(
+					{ type: type, command: "submit" },
+					vscode.TaskScope.Workspace,
+                    "submit",
+					"cargo-atcoder",
+					new vscode.ShellExecution(`cargo atcoder submit ${basename}`)
+				)
+            ];
+        },
+        resolveTask(task: vscode.Task, token?: vscode.CancellationToken) {
+            return task;
+        }
+    });
 }
 
 export function deactivate() {}
